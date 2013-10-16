9cde0387506944f89a10ac42759ef62cc20139f9
Ian Godin <iangodin@gmail.com>
2013-10-15 23:25:33 -0700

    Removed allegro from build.

--- build.lua
+++ build.lua
@@ -1,11 +1,10 @@
 
-Include( source_dir, ALLEG_INCLUDE, IMG_INCLUDE, MAIN_INCLUDE, PRIM_INCLUDE, FONT_INCLUDE, BOTAN_INCLUDE )
+Include( source_dir, SDL_INCLUDE, BOTAN_INCLUDE )
 
-SubDir( "allegro" )
 SubDir( "core" )
 SubDir( "store" )
 SubDir( "layout" )
 SubDir( "platform" )
 
-Executable( "test_layout", Compile( "test_layout.cpp" ), LinkLibs( "core", "allegropp", "layout" ), LinkSys( ALLEG_LIBS, IMG_LIBS, MAIN_LIBS, PRIM_LIBS, FONT_LIBS, BOTAN_LIBS ) )
-Executable( "test", Compile( "test.cpp" ), LinkLibs( "core", "platform-sdl", "platform" ), LinkSys( SDL_LIBS ) )
+Executable( "test_layout", Compile( "test_layout.cpp" ), LinkLibs( "core", "layout", "platform-sdl", "platform" ), LinkSys( SDL_LIBS, BOTAN_LIBS ) )
+Executable( "test", Compile( "test.cpp" ), LinkLibs( "core", "platform-sdl", "platform" ), LinkSys( SDL_LIBS, BOTAN_LIBS ) )
