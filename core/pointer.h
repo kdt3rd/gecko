711fdc3b20bc3921c130f1b20bb128de8f115277
Ian Godin <iangodin@gmail.com>
2013-10-19 14:40:08 -0700

    Added help function to wrap c pointers with automatic free.

---
+++ core/pointer.h
@@ -0,0 +1,30 @@
+
+#pragma once
+
+#include <stdlib.h>
+#include <memory>
+
+namespace core
+{
+
+////////////////////////////////////////
+
+struct free_deleter
+{
+	void operator()( void *ptr )
+	{
+		::free( ptr );
+	}
+};
+
+////////////////////////////////////////
+
+template<typename T>
+std::unique_ptr<T,free_deleter> wrap_cptr( T *ptr )
+{
+	return std::unique_ptr<T,free_deleter>( ptr );
+}
+
+////////////////////////////////////////
+
+}
