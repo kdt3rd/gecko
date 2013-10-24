02b0ecf0ef200976b7a19ed2526717f82a3df175
Ian Godin <iangodin@gmail.com>
2013-10-23 21:14:16 -0700

    Added distance function to point.

--- draw/point.h
+++ draw/point.h
@@ -1,6 +1,8 @@
 
 #pragma once
 
+#include <cmath>
+
 namespace draw
 {
 
@@ -29,6 +31,13 @@ public:
 
 	void set_x( double xx ) { _x = xx; }
 	void set_y( double yy ) { _y = yy; }
+
+	static double distance( const point &p1, const point &p2 )
+	{
+		double dx = p1.x() - p2.x();
+		double dy = p1.y() - p2.y();
+		return std::sqrt( dx * dx + dy * dy );
+	}
 
 private:
 	double _x = 0.0, _y = 0.0;
