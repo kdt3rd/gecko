57d040abec60afafb5ad8f6092e25714ba73a0cf
Ian Godin <iangodin@gmail.com>
2013-10-26 13:21:37 -0700

    Added docs.

--- draw/canvas.h
+++ draw/canvas.h
@@ -30,15 +30,22 @@ public:
 	virtual void fill( const paint &p ) = 0;
 
 	/// @brief Draw a path
-	///
+	/// Draw a path on the canvas
 	/// @param p Path to draw
-	/// @param c Paint to apply to path
+	/// @param c Paint to apply to the path
 	virtual void draw_path( const path &p, const paint &c ) = 0;
+
+	/// @brief Draw text
+	/// Draw text on the canvas
+	/// @param font The font to draw with
+	/// @param p The start of the baseline
+	/// @param utf8 The text to draw
+	/// @param c The paint to color with
+	virtual void draw_text( const std::shared_ptr<font> &font, const point &p, const std::string &utf8, const paint &c ) = 0;
 
 	/// @brief Present the canvas
 	/// Finalize the drawing of the canvas and display it.
 	virtual void present( void );
-
 };
 
 ////////////////////////////////////////
