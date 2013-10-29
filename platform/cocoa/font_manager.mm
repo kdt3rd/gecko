fe0da5bdbc2e3f6d16934fa5424166c200567e7d
Ian Godin <iangodin@gmail.com>
2013-10-28 21:45:59 -0700

    Got font manager working in Cocoa (mostly).

--- platform/cocoa/font_manager.mm
+++ platform/cocoa/font_manager.mm
@@ -1,4 +1,9 @@
 
+#import <AppKit/NSFontManager.h>
+#import <Foundation/NSString.h>
+#import <ApplicationServices/ApplicationServices.h>
+#import <CoreText/CTFont.h>
+
 #include "font_manager.h"
 
 #include <iostream>
@@ -6,6 +11,7 @@
 #include <sstream>
 
 #include <draw/cairo/font.h>
+#include <cairo/cairo-quartz.h>
 
 namespace cocoa
 {
@@ -35,6 +41,10 @@ std::set<std::string> font_manager::get_foundries( void )
 std::set<std::string> font_manager::get_families( void )
 {
 	std::set<std::string> ret;
+
+	NSFontManager *fmgr = [NSFontManager sharedFontManager];
+	for ( id name in [fmgr availableFontFamilies] )
+		ret.insert( [(NSString*)name UTF8String] );
 	return ret;
 }
 
@@ -43,6 +53,14 @@ std::set<std::string> font_manager::get_families( void )
 std::set<std::string> font_manager::get_styles( void )
 {
 	std::set<std::string> ret;
+	ret.insert( "Italic" );
+	ret.insert( "Bold" );
+	ret.insert( "Narrow" );
+	ret.insert( "Expanded" );
+	ret.insert( "Condensed" );
+	ret.insert( "Small Caps" );
+	ret.insert( "Compressed" );
+	ret.insert( "Fixed Pitch" );
 	return ret;
 }
 
@@ -51,6 +69,17 @@ std::set<std::string> font_manager::get_styles( void )
 std::shared_ptr<draw::font> font_manager::get_font( const std::string &family, const std::string &style, double pixsize )
 {
 	std::shared_ptr<cairo::font> ret;
+
+	NSFontManager *fmgr = [NSFontManager sharedFontManager];
+	NSString *fam = [[NSString alloc] initWithUTF8String:family.c_str()];
+	NSFontTraitMask mask = 0;
+	NSFont *nsfont = [fmgr fontWithFamily:fam traits:mask weight:5 size:pixsize];
+	if ( nsfont )
+	{
+		CGFontRef font = CTFontCopyGraphicsFont( (CTFontRef)nsfont, NULL );
+		ret = std::make_shared<cairo::font>( cairo_quartz_font_face_create_for_cgfont( font ), family, style, pixsize );
+	}
+	
 	return ret;
 }
 
