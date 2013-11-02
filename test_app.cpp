8e7f8d9eacb4de07a5c1d394963e2e6461828a45
Ian Godin <iangodin@gmail.com>
2013-11-02 15:26:22 -0700

    Added multiple window with many layouts.

--- test_app.cpp
+++ test_app.cpp
@@ -1,5 +1,6 @@
 
 #include <layout/form_layout.h>
+#include <layout/box_layout.h>
 #include <gui/application.h>
 #include <gui/cocoa_style.h>
 
@@ -7,7 +8,7 @@ namespace {
 
 ////////////////////////////////////////
 
-void build_window( const std::shared_ptr<gui::window> &win )
+void build_form_window( const std::shared_ptr<gui::window> &win )
 {
 	using layout::form_layout;
 
@@ -21,8 +22,24 @@ void build_window( const std::shared_ptr<gui::window> &win )
 	builder.make_button( row.second, "Press Me" );
 
 	row = layout->new_row();
-	builder.make_label( row.first, "Hello World" );
-	builder.make_button( row.second, "Hello World" );
+	builder.make_label( row.first, "Goodbye World" );
+	builder.make_button( row.second, "Me Too" );
+}
+
+void build_box_window( const std::shared_ptr<gui::window> &win )
+{
+	using layout::box_layout;
+
+	gui::builder builder( win );
+	auto layout = builder.new_layout<box_layout>( layout::direction::DOWN );
+	layout->set_pad( 12.0, 12.0, 12.0, 12.0 );
+	layout->set_spacing( 12.0, 12.0 );
+
+	builder.make_label( layout->new_area(), "Hello World" );
+	builder.make_button( layout->new_area(), "Press Me" );
+
+	builder.make_label( layout->new_area(), "Goodbye World" );
+	builder.make_button( layout->new_area(), "Me Too" );
 }
 
 ////////////////////////////////////////
@@ -33,9 +50,14 @@ int safemain( int argc, char **argv )
 	app->push();
 	app->set_style( std::make_shared<gui::cocoa_style>() );
 
-	auto win = app->new_window();
-	build_window( win );
-	win->show();
+	auto win1 = app->new_window();
+	build_form_window( win1 );
+	win1->show();
+
+	auto win2 = app->new_window();
+	build_box_window( win2 );
+	win2->show();
+
 	int code = app->run();
 	app->pop();
 	return code;
