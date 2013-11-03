b717d506ecd9e6a32dc44123e7f2a2fdc22d39b6
Ian Godin <iangodin@gmail.com>
2013-11-02 20:39:28 -0700

    Added more documentation.

--- platform/dispatcher.h
+++ platform/dispatcher.h
@@ -23,6 +23,10 @@ public:
 	/// The dispatcher will begin sending out events until the exit() method is called.
 	/// @return The exit code
 	virtual int execute( void ) = 0;
+
+	/// @brief Cause execute to stop.
+	/// The execute method will return the given code.
+	/// @param code Exit code
 	virtual void exit( int code ) = 0;
 };
 
--- platform/font_manager.h
+++ platform/font_manager.h
@@ -14,15 +14,40 @@ namespace platform
 class font_manager
 {
 public:
+	/// @brief Constructor
 	font_manager( void );
+
+	/// @brief Destructor
 	virtual ~font_manager( void );
 
+	/// @brief Name of font manager
+	/// The name of the font manager being used.
+	/// @return The name of the font manager
 	const std::string &name( void ) const { return _name; }
+
+	/// @brief Version of the font manager
+	/// The version of the font manager being used.
+	/// @return The version of the font manager
 	const std::string &version( void ) const { return _version; }
 
+
+	/// @brief List of families
+	/// Get a list of families of all managed fonts.
+	/// @return The list of families
 	virtual std::set<std::string> get_families( void ) = 0;
+
+	/// @brief List of style
+	/// Get a list of styles available.
+	/// @return The list of styles.
 	virtual std::set<std::string> get_styles( void ) = 0;
 
+	/// @brief Get a particular font
+	/// Create and return a font with the given attributes.
+	/// @param family Family of the font to create
+	/// @param style Style of the font to create
+	/// @param pixsize Size of the font to create
+	///
+	/// @return The best matching font found
 	virtual std::shared_ptr<draw::font> get_font( const std::string &family, const std::string &style, double pixsize ) = 0;
 
 protected:
