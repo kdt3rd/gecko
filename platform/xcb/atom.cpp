fb639711299b2981533cdcdb3254aa3f0455b528
Ian Godin <iangodin@gmail.com>
2013-11-07 20:19:32 -0800

    Added start of xcb C++ wrapper for cookie/reply.

---
+++ platform/xcb/atom.cpp
@@ -0,0 +1,34 @@
+
+#include "atom.h"
+#include <core/pointer.h>
+#include <core/contract.h>
+#include "xcbpp.h"
+
+namespace xcb
+{
+
+////////////////////////////////////////
+
+std::string get_atom_name( xcb_connection_t *c, xcb_atom_t a )
+{
+	auto reply = xcbpp_get_atom_name( c, a );
+
+	size_t len = xcb_get_atom_name_name_length( reply.get() );
+	return std::string( xcb_get_atom_name_name( reply.get() ), len );
+}
+
+////////////////////////////////////////
+
+xcb_atom_t get_atom( xcb_connection_t *c, const std::string &name )
+{
+	xcb_atom_t atom = XCB_NONE;
+	auto reply = xcbpp_intern_atom( c, 0, name.size(), name.c_str() );
+	if ( reply )
+		atom = reply->atom;
+	return atom;
+}
+
+////////////////////////////////////////
+
+}
+
