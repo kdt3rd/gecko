b9ce06539028d8d2e49b3c81609a38d6dde6f218
Ian Godin <iangodin@gmail.com>
2013-11-08 16:20:25 -0800

    Added alignment to text view.

--- view/text.cpp
+++ view/text.cpp
@@ -36,9 +36,55 @@ void text::paint( const std::shared_ptr<draw::canvas> &canvas )
 	draw::font_extents fex = _font->extents();
 	draw::text_extents tex = _font->extents( _text );
 
+	draw::rect a = _area->rectangle();
+	double y = 0.0, x = 0.0;
+
+	switch ( _align )
+	{
+		case alignment::CENTER:
+		case alignment::LEFT:
+		case alignment::RIGHT:
+			y = a.y() + ( a.height() + fex.ascent ) / 2.0;
+			break;
+
+		case alignment::BOTTOM:
+		case alignment::BOTTOM_RIGHT:
+		case alignment::BOTTOM_LEFT:
+			y = a.y2() - fex.descent;
+			break;
+
+		case alignment::TOP:
+		case alignment::TOP_RIGHT:
+		case alignment::TOP_LEFT:
+			y = a.y1() + fex.ascent;
+			break;
+	}
+
+	switch ( _align )
+	{
+		case alignment::LEFT:
+		case alignment::TOP_LEFT:
+		case alignment::BOTTOM_LEFT:
+			x = a.x() - tex.x_bearing;
+			break;
+
+		case alignment::RIGHT:
+		case alignment::TOP_RIGHT:
+		case alignment::BOTTOM_RIGHT:
+			x = a.x2() - tex.width;
+			break;
+
+		case alignment::CENTER:
+		case alignment::TOP:
+		case alignment::BOTTOM:
+			x = a.x1() + ( a.width() - tex.width ) /2.0;
+			break;
+
+	}
+
 	draw::paint paint;
 	paint.set_fill_color( _color );
-	canvas->draw_text( _font, { _area->x() - tex.x_bearing, _area->y() + fex.ascent }, _text, paint );
+	canvas->draw_text( _font, { x, y }, _text, paint );
 }
 
 ////////////////////////////////////////
--- view/text.h
+++ view/text.h
@@ -1,6 +1,7 @@
 
 #pragma once
 
+#include <core/alignment.h>
 #include "view.h"
 
 namespace view
@@ -17,11 +18,13 @@ public:
 	void set_font( const std::shared_ptr<draw::font> &fnt ) { _font = fnt; }
 	void set_color( const draw::color &c ) { _color = c; }
 	void set_text( std::string txt ) { _text = std::move( txt ); }
+	void set_alignment( alignment a ) { _align = a; }
 
 	virtual void layout( const std::shared_ptr<draw::canvas> &canvas );
 	virtual void paint( const std::shared_ptr<draw::canvas> &canvas );
 
 private:
+	alignment _align = alignment::CENTER;
 	std::shared_ptr<draw::area> _area;
 	std::shared_ptr<draw::font> _font;
 	draw::color _color;
