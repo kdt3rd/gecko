42f1f2c199e87c8c2edf6c292760fa62104e5b38
Ian Godin <iangodin@gmail.com>
2013-11-28 16:18:54 -0800

    Misc. clean up for box layout.

--- layout/box_layout.h
+++ layout/box_layout.h
@@ -19,8 +19,8 @@ class box_layout : public layout<container,area>
 {
 public:
 	box_layout( direction dir = direction::DOWN )
+		: _dir( dir )
 	{
-		set_direction( dir );
 	}
 
 	void set_direction( direction d )
@@ -83,41 +83,31 @@ public:
 		switch ( _dir )
 		{
 			case direction::LEFT:
-				xflow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[0], _pad[1] );
+				flow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[0], _pad[1] );
 				overlap_constraint( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
 				break;
 
 			case direction::RIGHT:
-				xflow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[1], _pad[0] );
+				flow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[1], _pad[0] );
 				overlap_constraint( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
 				break;
 
 			case direction::UP:
-				xflow_constraint( _areas, _weights, master, _dir, _vspacing, _pad[2], _pad[3] );
+				flow_constraint( _areas, _weights, master, _dir, _vspacing, _pad[2], _pad[3] );
 				overlap_constraint( _areas, master, orientation::HORIZONTAL, _pad[0], _pad[1] );
 				break;
 
 			case direction::DOWN:
-				xflow_constraint( _areas, _weights, master, _dir, _vspacing, _pad[2], _pad[3] );
+				flow_constraint( _areas, _weights, master, _dir, _vspacing, _pad[2], _pad[3] );
 				overlap_constraint( _areas, master, orientation::HORIZONTAL, _pad[0], _pad[1] );
 				break;
 		}
 	}
 
-	typename std::vector<std::shared_ptr<area>>::iterator begin( void )
-	{
-		return _areas.begin();
-	}
-
-	typename std::vector<std::shared_ptr<area>>::iterator end( void )
-	{
-		return _areas.end();
-	}
-
-private:
+protected:
 	direction _dir = direction::DOWN;
-	double _pad[4];
-	double _hspacing, _vspacing;
+	double _pad[4] = { 0.0, 0.0, 0.0, 0.0 };
+	double _hspacing = 0.0, _vspacing = 0.0;
 	std::vector<std::shared_ptr<area>> _areas;
 	std::vector<double> _weights;
 };
