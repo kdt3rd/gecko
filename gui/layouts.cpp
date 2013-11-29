de3983b61a21834dcc14ce6af2ee21d00a41aeee
Ian Godin <iangodin@gmail.com>
2013-11-28 19:06:21 -0800

    Converted grid layout to use new constraints.

--- gui/layouts.cpp
+++ gui/layouts.cpp
@@ -6,7 +6,7 @@ namespace layout
 
 template class box_layout<gui::widget,gui::widget>;
 template class form_layout<gui::widget,gui::widget>;
-//template class grid_layout<gui::widget,gui::widget>;
+template class grid_layout<gui::widget,gui::widget>;
 //template class tree_layout<gui::widget,gui::widget>;
 
 }
--- layout/grid_layout.h
+++ layout/grid_layout.h
@@ -3,8 +3,7 @@
 
 #include <memory>
 #include "layout.h"
-#include "tight_constraint.h"
-#include "flow_constraint.h"
+#include "constraint.h"
 
 namespace layout
 {
@@ -17,89 +16,180 @@ namespace layout
 template<typename container, typename area>
 class grid_layout : public layout<container, area>
 {
-	typedef tight_constraint<simple_area,area> tconstraint;
-
 public:
-	grid_layout( void );
-
-	void add_row( const std::vector<std::shared_ptr<area>> &add, double w )
+	grid_layout( void )
 	{
-		precondition( add.size() == _columns.cells.size(), "invalid row size" );
-		_rows.cells.emplace_back( std::make_pair( std::make_shared<tconstraint>( orientation::VERTICAL ), std::make_shared<simple_area>() ) );
-		_rows.box.add_area( _rows.cells.back().second, w );
-		auto &row = _rows.cells.back();
-		for ( size_t i = 0; i < add.size(); ++i )
-		{
-			row.first->add_area( add[i] );
-			_columns.cells[i].first->add_area( add[i] );
-		}
-	}
-
-	void add_column( const std::vector<std::shared_ptr<area>> &add, double w )
-	{
-		precondition( add.size() == _rows.cells.size(), "invalid columns size" );
-		_columns.cells.emplace_back( std::make_pair( std::make_shared<tconstraint>( orientation::HORIZONTAL ), std::make_shared<simple_area>() ) );
-		_columns.box.add_area( _columns.cells.back().second, w );
-		auto &col = _columns.cells.back().first;
-		for ( size_t i = 0; i < add.size(); ++i )
-		{
-			col->add_area( add[i] );
-			_rows.cells[i].first->add_area( add[i] );
-		}
 	}
 
 	virtual void set_pad( double left, double right, double top, double bottom )
 	{
-		_rows.box.set_pad( left, right );
-		_columns.box.set_pad( top, bottom );
+		_pad[0] = left;
+		_pad[1] = right;
+		_pad[2] = top;
+		_pad[3] = bottom;
 	}
 
 	virtual void set_spacing( double horiz, double vert )
 	{
-		_rows.box.set_spacing( horiz );
-		_columns.box.set_spacing( vert );
+		_hspacing = horiz;
+		_vspacing = vert;
+	}
+
+	void add_row( const std::vector<std::shared_ptr<area>> &add, double w = 0.0 )
+	{
+		if ( _rows == 0 && _cols == 0 )
+		{
+			_cols = add.size();
+			for ( size_t i = 0; i < _cols; ++i )
+			{
+				_careas.push_back( std::make_shared<simple_area>() );
+				_cweights.push_back( 0.0 );
+			}
+		}
+
+		precondition( add.size() == _cols, "invalid row size" );
+		_rareas.push_back( std::make_shared<simple_area>() );
+		_rweights.push_back( w );
+		_rows++;
+		_areas.push_back( add );
+
+		for ( auto &a: add )
+			this->added( a );
+	}
+
+	void add_column( const std::vector<std::shared_ptr<area>> &add, double w = 0.0 )
+	{
+		if ( _rows == 0 && _cols == 0 )
+		{
+			_rows = add.size();
+			for ( size_t i = 0; i < _rows; ++i )
+			{
+				_rareas.push_back( std::make_shared<simple_area>() );
+				_rweights.push_back( 0.0 );
+			}
+		}
+
+		precondition( add.size() == _rows, "invalid column size" );
+		_careas.push_back( std::make_shared<simple_area>() );
+		_cweights.push_back( w );
+		_cols++;
+
+		for ( size_t i = 0; i < _areas.size(); ++i )
+		{
+			_areas[i].push_back( add[i] );
+			this->added( add[i] );
+		}
 	}
 
 	virtual void recompute_minimum( container &master )
 	{
-		for ( auto r: _rows.cells )
-			r.first->recompute_minimum( *r.second );
-		for ( auto c: _columns.cells )
-			c.first->recompute_minimum( *c.second );
+		for ( auto &r: _areas )
+		{
+			for ( auto &a: r )
+				a->layout();
+		}
 
-		_rows.box.recompute_minimum( master );
-		_columns.box.recompute_minimum( master );
+		switch ( _dir )
+		{
+			case direction::LEFT:
+				/*
+				flow_minimum( _areas, master, _dir, _hspacing, _pad[0], _pad[1] );
+				overlap_minimum( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
+				*/
+				break;
+
+			case direction::RIGHT:
+				/*
+				flow_minimum( _areas, master, _dir, _hspacing, _pad[1], _pad[0] );
+				overlap_minimum( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
+				*/
+				break;
+
+			case direction::UP:
+				for ( size_t c = 0; c < _careas.size(); ++c )
+					overlap_minimum( column( _areas, c ), *_careas[c], orientation::HORIZONTAL );
+
+				for ( size_t r = 0; r < _rareas.size(); ++r )
+					overlap_minimum( _areas[r], *_rareas[r], orientation::VERTICAL );
+
+				flow_minimum( _careas, master, direction::RIGHT, _hspacing, _pad[0], _pad[1] );
+				flow_minimum( _rareas, master, direction::UP, _vspacing, _pad[2], _pad[3] );
+				break;
+
+			case direction::DOWN:
+				for ( size_t c = 0; c < _careas.size(); ++c )
+					overlap_minimum( column( _areas, c ), *_careas[c], orientation::HORIZONTAL );
+
+				for ( size_t r = 0; r < _rareas.size(); ++r )
+					overlap_minimum( _areas[r], *_rareas[r], orientation::VERTICAL );
+				flow_minimum( _careas, master, direction::RIGHT, _hspacing, _pad[0], _pad[1] );
+				flow_minimum( _rareas, master, direction::DOWN, _vspacing, _pad[2], _pad[3] );
+				break;
+		}
 	}
 
 
 	virtual void recompute_layout( container &master )
 	{
-		_rows.box.recompute_constraint( master );
-		_columns.box.recompute_constraint( master );
+		switch ( _dir )
+		{
+			case direction::LEFT:
+				/*
+				xflow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[0], _pad[1] );
+				overlap_constraint( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
+				*/
+				break;
 
-		for ( auto r: _rows.cells )
-			r.first->recompute_constraint( *r.second );
-		for ( auto c: _columns.cells )
-			c.first->recompute_constraint( *c.second );
+			case direction::RIGHT:
+				/*
+				xflow_constraint( _areas, _weights, master, _dir, _hspacing, _pad[1], _pad[0] );
+				overlap_constraint( _areas, master, orientation::VERTICAL, _pad[2], _pad[3] );
+				*/
+				break;
+
+			case direction::UP:
+			{
+				flow_constraint( _careas, _cweights, master, direction::RIGHT, _hspacing, _pad[0], _pad[1] );
+				for ( size_t c = 0; c < _careas.size(); ++c )
+					overlap_constraint( column( _areas, c ), *_careas[c], orientation::HORIZONTAL );
+
+				flow_constraint( _rareas, _rweights, master, direction::UP, _vspacing, _pad[2], _pad[3] );
+				for ( size_t r = 0; r < _rareas.size(); ++r )
+					overlap_constraint( _areas[r], *_rareas[r], orientation::VERTICAL );
+				break;
+			}
+
+			case direction::DOWN:
+			{
+				flow_constraint( _careas, _cweights, master, direction::RIGHT, _hspacing, _pad[0], _pad[1] );
+				for ( size_t c = 0; c < _careas.size(); ++c )
+					overlap_constraint( column( _areas, c ), *_careas[c], orientation::HORIZONTAL );
+
+				flow_constraint( _rareas, _rweights, master, direction::DOWN, _vspacing, _pad[2], _pad[3] );
+				for ( size_t r = 0; r < _rareas.size(); ++r )
+					overlap_constraint( _areas[r], *_rareas[r], orientation::VERTICAL );
+
+				break;
+			}
+		}
 	}
 
 private:
-	std::shared_ptr<area> _container;
+	typedef std::vector<std::shared_ptr<area>> row;
 
-	struct group
-	{
-		group( void )
-			: a( std::make_shared<simple_area>() )
-		{
-		}
+	direction _dir = direction::DOWN;
+	double _pad[4] = { 0.0, 0.0, 0.0, 0.0 };
+	double _hspacing = 0.0, _vspacing = 0.0;
 
-		std::shared_ptr<simple_area> a;
-		flow_constraint<container,simple_area> box;
-		std::vector<std::pair<std::shared_ptr<tconstraint>,std::shared_ptr<simple_area>>> cells;
-	};
+	std::vector<row> _areas;
 
-	group _rows;
-	group _columns;
+	std::vector<std::shared_ptr<simple_area>> _rareas;
+	std::vector<double> _rweights;
+
+	std::vector<std::shared_ptr<simple_area>> _careas;
+	std::vector<double> _cweights;
+
+	size_t _rows = 0, _cols = 0;
 };
 
 ////////////////////////////////////////
