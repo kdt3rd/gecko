862b0cce124fe6e026327bdd20305fa89e099fe6
Ian Godin <iangodin@gmail.com>
2013-12-23 14:39:42 -0800

    Added tree layout test.

--- test_app.cpp
+++ test_app.cpp
@@ -11,15 +11,17 @@
 #include <gui/label.h>
 #include <gui/button.h>
 #include <gui/slider.h>
+#include <gui/color.h>
 #include <gui/scroll_area.h>
+#include <gui/tree_node.h>
 
 namespace {
 
 ////////////////////////////////////////
 
-std::shared_ptr<gui::widget> build_form( void )
+std::shared_ptr<gui::widget> build_form( direction dir )
 {
-	auto container = std::make_shared<gui::form>( direction::DOWN );
+	auto container = std::make_shared<gui::form>( dir );
 	container->set_spacing( 12, 6 );
 	container->set_pad( 12.5, 12.5, 12.5, 12.5 );
 	container->add( std::make_shared<gui::label>( "Hello World" ), std::make_shared<gui::button>( "Click Me" ) );
@@ -29,9 +31,9 @@ std::shared_ptr<gui::widget> build_form( void )
 
 ////////////////////////////////////////
 
-std::shared_ptr<gui::widget> build_grid( void )
+std::shared_ptr<gui::widget> build_grid( direction dir )
 {
-	auto container = std::make_shared<gui::grid>();
+	auto container = std::make_shared<gui::grid>( dir );
 	container->set_spacing( 12, 6 );
 	container->set_pad( 12.5, 12.5, 12.5, 12.5 );
 
@@ -56,9 +58,9 @@ std::shared_ptr<gui::widget> build_grid( void )
 
 ////////////////////////////////////////
 
-std::shared_ptr<gui::widget> build_box( void )
+std::shared_ptr<gui::widget> build_box( direction dir )
 {
-	auto container = std::make_shared<gui::simple_container>( direction::DOWN );
+	auto container = std::make_shared<gui::simple_container>( dir );
 	container->set_spacing( 12, 6 );
 	container->set_pad( 12.5, 12.5, 12.5, 12.5 );
 
@@ -72,6 +74,43 @@ std::shared_ptr<gui::widget> build_box( void )
 
 	return container;
 }
+////////////////////////////////////////
+
+std::shared_ptr<gui::widget> build_tree( direction dir )
+{
+	auto container = std::make_shared<gui::tree_node>( 12.0, dir );
+//	container->set_root( std::make_shared<gui::label>( "Root" ) );
+	container->set_root( std::make_shared<gui::color>( draw::color( { 0, 0, 1 } ), 12, 12 ) );
+	container->set_tab( std::make_shared<gui::color>( draw::color( { 1, 0, 0 } ) ) );
+	container->set_spacing( 12, 6 );
+	container->set_pad( 12.5, 12.5, 12.5, 12.5 );
+
+	std::string name( "A" );
+	for ( size_t y = 0; y < 3; ++y )
+	{
+		auto node = std::make_shared<gui::tree_node>( 12.0, direction::DOWN );
+		if( y == 1 )
+		{
+			auto button = std::make_shared<gui::button>( "Collapse" );
+			node->set_root( button );
+		}
+		else
+			node->set_root( std::make_shared<gui::label>( name ) );
+		node->set_tab( std::make_shared<gui::color>() );
+		std::string sub( "1" );
+		for ( size_t x = 0; x < 3; ++x )
+		{
+			node->add( std::make_shared<gui::label>( sub ) );
+			node->set_spacing( 12, 6 );
+			++sub[0];
+		}
+		container->add( node );
+		++name[0];
+	}
+
+	return container;
+}
+
 
 ////////////////////////////////////////
 
@@ -87,12 +126,27 @@ int safemain( int argc, char **argv )
 	if ( argc > 1 )
 		test = argv[1];
 
+	std::string dirname = "box";
+	if ( argc > 2 )
+		dirname = argv[2];
+	direction dir = direction::DOWN;
+	if ( dirname == "down" )
+		dir = direction::DOWN;
+	else if ( dirname == "up" )
+		dir = direction::UP;
+	else if ( dirname == "left" )
+		dir = direction::LEFT;
+	else if ( dirname == "right" )
+		dir = direction::RIGHT;
+
 	if ( test == "form" )
-		win->set_widget( build_form() );
+		win->set_widget( build_form( dir ) );
 	else if ( test == "grid" )
-		win->set_widget( build_grid() );
+		win->set_widget( build_grid( dir ) );
 	else if ( test == "box" )
-		win->set_widget( build_box() );
+		win->set_widget( build_box( dir ) );
+	else if ( test == "tree" )
+		win->set_widget( build_tree( dir ) );
 
 	win->show();
 
