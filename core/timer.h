e535657831e95c4eb554c420318c1c844b1f71aa
Ian Godin <iangodin@gmail.com>
2013-12-31 09:13:05 -0800

    Added timer class.

---
+++ core/timer.h
@@ -0,0 +1,37 @@
+
+#pragma once
+
+#include <chrono>
+
+namespace core
+{
+
+////////////////////////////////////////
+
+class timer
+{
+public:
+	explicit timer( bool run = false )
+	{
+		if ( run )
+			start();
+	}
+
+	void start( void )
+	{
+		_start = std::chrono::high_resolution_clock::now();
+	}
+
+	std::chrono::milliseconds elapsed( void ) const
+	{
+		return std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - _start );
+	}
+
+private:
+	std::chrono::high_resolution_clock::time_point _start;
+};
+
+////////////////////////////////////////
+
+}
+
