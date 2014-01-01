48c0d88b5dc6dbc8cc603152cfca27e28d8dc249
Ian Godin <iangodin@gmail.com>
2013-12-31 16:10:18 -0800

    Added method to convert enum to integer.

--- core/meta.h
+++ core/meta.h
@@ -4,3 +4,9 @@
 template<typename A, typename B>
 using disable_if_same_or_derived = typename std::enable_if<!std::is_base_of<A,typename std::remove_reference<B>::type>::value>::type;
 
+template<typename E>
+auto as_integer( E const v ) -> typename std::underlying_type<E>::type
+{
+	return static_cast<typename std::underlying_type<E>::type>( v );
+}
+
