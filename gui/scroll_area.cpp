9220f18a6cad9f132aad79f74af612bf2abb6d5a
Ian Godin <iangodin@gmail.com>
2014-01-02 22:27:09 -0800

    Fixed up scroll_area to mostly working.

--- gui/scroll_area.cpp
+++ gui/scroll_area.cpp
@@ -6,15 +6,9 @@ namespace gui
 
 ////////////////////////////////////////
 
-scroll_area::scroll_area( void )
+scroll_area::scroll_area( scroll_behavior hscroll, scroll_behavior vscroll )
+	: _hscroll( hscroll ), _vscroll( vscroll )
 {
-}
-
-////////////////////////////////////////
-
-scroll_area::scroll_area( double w, double h )
-{
-	set_minimum( w, h );
 }
 
 ////////////////////////////////////////
@@ -27,42 +21,163 @@ scroll_area::~scroll_area( void )
 
 void scroll_area::paint( const std::shared_ptr<draw::canvas> &canvas )
 {
-	canvas->save();
-	canvas->clip( *this );
-	canvas->translate( -_position.x(), -_position.y() );
-	_widget->paint( canvas );
-	canvas->restore();
+	if ( _widget )
+	{
+		canvas->save();
+		canvas->clip( *this );
+		_widget->paint( canvas );
+		canvas->restore();
+	}
 }
 
 ////////////////////////////////////////
 
 bool scroll_area::mouse_press( const draw::point &p, int button )
 {
-	return _widget->mouse_press( p + _position, button );
+	if ( button == 2 && _widget )
+	{
+		_tracking = true;
+		_track = p;
+		return true;
+	}
+	else
+	{
+		if ( _widget )
+			return _widget->mouse_press( p, button );
+		return widget::mouse_press( p, button );
+	}
 }
 
 ////////////////////////////////////////
 
 bool scroll_area::mouse_release( const draw::point &p, int button )
 {
-	return _widget->mouse_release( p + _position, button );
+	if ( _tracking )
+	{
+		_tracking = false;
+		return true;
+	}
+	else
+	{
+		if ( _widget )
+			return _widget->mouse_release( p, button );
+		return widget::mouse_release( p, button );
+	}
 }
 
 ////////////////////////////////////////
 
 bool scroll_area::mouse_move( const draw::point &p )
 {
-	return _widget->mouse_move( p + _position );
+	if ( _tracking )
+	{
+		if ( _widget )
+			_widget->set_position( _widget->position() - _track.delta( p ) );
+		compute_layout();
+		_track = p;
+		invalidate();
+		return true;
+	}
+	else
+	{
+		if ( _widget )
+			return _widget->mouse_move( p );
+		return widget::mouse_move( p );
+	}
+}
+
+////////////////////////////////////////
+
+void scroll_area::compute_minimum( void )
+{
+	if ( _widget )
+	{
+		_widget->compute_minimum();
+		switch( _hscroll )
+		{
+			case scroll_behavior::FREE:
+			case scroll_behavior::BOUND:
+				break;
+
+			case scroll_behavior::NONE:
+				set_minimum_width( _widget->minimum_width() );
+				break;
+		}
+		switch( _vscroll )
+		{
+			case scroll_behavior::FREE:
+			case scroll_behavior::BOUND:
+				break;
+
+			case scroll_behavior::NONE:
+				set_minimum_height( _widget->minimum_height() );
+				break;
+		}
+	}
 }
 
 ////////////////////////////////////////
 
 void scroll_area::compute_layout( void )
 {
-	_widget->compute_minimum();
-	_widget->set_horizontal( 0.0, std::max( _widget->minimum_width(), width() ) - 1.0 );
-	_widget->set_vertical( 0.0, std::max( _widget->minimum_height(), height() ) - 1.0 );
-	_widget->compute_layout();
+	if ( _widget )
+	{
+		switch( _hscroll )
+		{
+			case scroll_behavior::FREE:
+				_widget->set_width( _widget->minimum_width() );
+				break;
+
+			case scroll_behavior::BOUND:
+				_widget->set_width( _widget->minimum_width() );
+				if ( _widget->x() > x() )
+					_widget->set_x( x() );
+				if ( _widget->x2() < x2() )
+				{
+					if ( _widget->minimum_width() < width() )
+					{
+						_widget->set_x1( x1() );
+						_widget->set_x2( x2() );
+					}
+					else
+						_widget->move_by( _widget->x2() - x2(), 0 );
+				}
+				break;
+
+			case scroll_behavior::NONE:
+				_widget->set_x( x() );
+				_widget->set_width( width() );
+				break;
+		}
+		switch( _vscroll )
+		{
+			case scroll_behavior::FREE:
+				_widget->set_height( _widget->minimum_height() );
+				break;
+
+			case scroll_behavior::BOUND:
+				_widget->set_height( _widget->minimum_height() );
+				if ( _widget->y() > y() )
+					_widget->set_y( y() );
+				if ( _widget->y2() < y2() )
+				{
+					if ( _widget->minimum_height() < height() )
+					{
+						_widget->set_y1( y1() );
+						_widget->set_y2( y2() );
+					}
+					else
+						_widget->move_by( 0, y2() - _widget->y2() );
+				}
+				break;
+
+			case scroll_behavior::NONE:
+				_widget->set_y( y() );
+				_widget->set_height( height() );
+				break;
+		}
+		_widget->compute_layout();
+	}
 }
 
 ////////////////////////////////////////
--- gui/scroll_area.h
+++ gui/scroll_area.h
@@ -8,11 +8,19 @@ namespace gui
 
 ////////////////////////////////////////
 
+enum class scroll_behavior
+{
+	FREE,
+	BOUND,
+	NONE
+};
+
+////////////////////////////////////////
+
 class scroll_area : public widget
 {
 public:
-	scroll_area( void );
-	scroll_area( double minw, double minh );
+	scroll_area( scroll_behavior hscroll = scroll_behavior::BOUND, scroll_behavior vscroll = scroll_behavior::BOUND );
 	~scroll_area( void );
 
 	void set_widget( const std::shared_ptr<widget> &v )
@@ -26,10 +34,14 @@ public:
 	bool mouse_release( const draw::point &p, int button ) override;
 	bool mouse_move( const draw::point &p ) override;
 
+	void compute_minimum( void ) override;
 	void compute_layout( void ) override;
 
 private:
-	draw::point _position;
+	bool _tracking = false;
+	draw::point _track;
+
+	scroll_behavior _hscroll = scroll_behavior::BOUND, _vscroll = scroll_behavior::BOUND;
 	std::shared_ptr<widget> _widget;
 };
 
