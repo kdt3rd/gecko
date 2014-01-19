720dfb92039cb1199a2c6d92ac51d1421b793774
Ian Godin <iangodin@gmail.com>
2014-01-18 16:13:44 -0800

    Show shader compile warnings for debugging.

--- gl/shader.cpp
+++ gl/shader.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <iostream>
 #include <stdexcept>
+#include <core/string.h>
 #include "shader.h"
 
 namespace gl
@@ -51,10 +52,26 @@ void shader::compile( void )
 	{
 		GLint len = 0;
 		glGetShaderiv( _shader, GL_SHADER_SOURCE_LENGTH, &len );
-		std::string source( len + 1, '\0' );
-		glGetShaderSource( _shader, len + 1, nullptr, &source[0] );
+		std::string source( len, '\0' );
+		glGetShaderSource( _shader, len, nullptr, &source[0] );
 		std::cerr << "Compile error:\n" << source << std::endl;
 		throw std::runtime_error( log() );
+	}
+	else
+	{
+		std::string l = core::trim( log() );
+		if ( !l.empty() )
+		{
+			GLint len = 0;
+			glGetShaderiv( _shader, GL_SHADER_SOURCE_LENGTH, &len );
+			std::string source( len+1, '\0' );
+			glGetShaderSource( _shader, len+1, nullptr, &source[0] );
+			std::cerr << "========================================" << std::endl;
+			std::cerr << "Compile warning:\n" << source << std::endl;
+			std::cerr << "----------------------------------------" << std::endl;
+			std::cerr << l << std::endl;
+			std::cerr << "========================================" << std::endl;
+		}
 	}
 }
 
@@ -65,7 +82,7 @@ std::string shader::log( void )
 	GLint res;
 	glGetShaderiv( _shader, GL_INFO_LOG_LENGTH, &res );
 
-	if ( res > 0 )
+	if ( res > 1 )
 	{
 		std::string info( res, 0 );
 		glGetShaderInfoLog( _shader, res, &res, &info[0] );
