aecf2cc9bdf3ef5dec78ed93276908cb6d2ec3ba
Ian Godin <iangodin@gmail.com>
2014-06-29 14:05:16 -0700

    Replaced glew with gl3w.

--- libs/gl/opengl.h
+++ libs/gl/opengl.h
@@ -6,12 +6,11 @@
 #endif
 
 #ifdef _WIN32
-#define GLEW_STATIC
-#	include <glew.h>
-#	include <wglew.h>
+#	include "gl3w.h"
 #endif
 
 #ifdef __unix__
-#	include <GL/glxew.h>
+#	include "gl3w.h"
+#	include <GL/glx.h>
 #endif
 
