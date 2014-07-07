80eef2f242307fe551fc10ab95917fdeebd9bbbc
Ian Godin <iangodin@gmail.com>
2014-07-06 17:02:23 -0700

    Started working on line edit.

--- libs/gui/line_edit.h
+++ libs/gui/line_edit.h
@@ -4,7 +4,7 @@
 #include "widget.h"
 #include "application.h"
 #include "style.h"
-#include <model/datum.h>
+#include <draw/stretchable.h>
 #include <base/alignment.h>
 
 namespace gui
@@ -16,14 +16,14 @@ class line_edit : public widget
 {
 public:
 	line_edit( void );
-	line_edit( datum<std::string> &&l, datum<alignment> &&a = alignment::LEFT, datum<base::color> &&c = { 0, 0, 0, 1 }, shared_datum<script::font> &&f = application::current_style()->default_font() );
+	line_edit( std::string l, base::alignment a = base::alignment::LEFT, const base::color &c = { 0, 0, 0, 1 }, const std::shared_ptr<script::font> &f = application::current()->get_default_font() );
 	~line_edit( void );
 
-	const std::string &text( void ) { return _text.value(); }
+	const std::string &text( void ) const { return _text; }
 	void set_text( const std::string &t ) { _text = t; }
 
 	void set_font( std::shared_ptr<script::font> &f ) { _font = f; }
-	void set_align( alignment a ) { _align = a; }
+	void set_align( base::alignment a ) { _align = a; }
 
 	void paint( const std::shared_ptr<draw::canvas> &c ) override;
 
@@ -35,10 +35,13 @@ public:
 
 private:
 	size_t _cursor = 0;
-	datum<std::string> _text;
-	datum<alignment> _align = alignment::LEFT;
-	datum<base::color> _color = { 0, 0, 0, 1 };
-	shared_datum<script::font> _font = application::current_style()->default_font();
+	std::string _text;
+	base::alignment _align = base::alignment::LEFT;
+	base::color _color = { 0, 0, 0, 1 };
+	std::shared_ptr<script::font> _font = application::current()->get_default_font();
+
+	std::shared_ptr<draw::stretchable> _frame;
+	std::shared_ptr<draw::stretchable> _marker;
 };
 
 ////////////////////////////////////////
