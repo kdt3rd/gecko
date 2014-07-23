516a01ba56ede1e3d5ed0afae2b424be0f7072d9
Ian Godin <iangodin@gmail.com>
2014-07-22 23:33:09 -0700

    Added missing header file.

---
+++ libs/base/ansi.h
@@ -0,0 +1,25 @@
+
+#pragma once
+
+namespace base
+{
+
+namespace ansi
+{
+	constexpr const char *reset = "\x1B[0m";
+
+	constexpr const char *black = "\x1B[30m";
+	constexpr const char *red = "\x1B[31m";
+	constexpr const char *green = "\x1B[32m";
+	constexpr const char *yellow = "\x1B[33m";
+	constexpr const char *blue = "\x1B[34m";
+	constexpr const char *magenta = "\x1B[35m";
+	constexpr const char *cyan = "\x1B[36m";
+	constexpr const char *white = "\x1B[37m";
+
+	constexpr const char *underline = "\x1B[4m";
+	constexpr const char *invert = "\x1B[7m";
+}
+
+}
+
