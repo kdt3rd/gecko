17de849c029f2fdfa5db66b7e79c390261468d2c
Ian Godin <iangodin@gmail.com>
2014-10-24 21:54:44 -0700

    Added missing file.

---
+++ apps/imgc/newtest.cpp
@@ -0,0 +1,58 @@
+
+
+#include <imgproc/expr.h>
+#include <imgproc/environment.h>
+#include <imgproc/function.h>
+
+namespace
+{
+
+int safemain( int argc, char *argv[] )
+{
+	using namespace imgproc;
+
+	auto x = std::make_shared<expr>( identifier_expr( U"x" ) );
+	auto y = std::make_shared<expr>( identifier_expr( U"y" ) );
+	auto e = std::make_shared<expr>( infix_expr( U"+", x, y ) );
+
+	function f( U"add", U"x", U"y" );
+	f.set_result( e );
+	auto args = { var_type( pod_type::FLOAT32, 0 ), var_type( pod_type::FLOAT32, 0 ) };
+
+
+	std::cout << e << " = " << infer( f, args )->get_type() << std::endl;
+
+	/*
+	environment env;
+	env[U"true"] = make_type<bool>();
+	env[U"false"] = make_type<bool>();
+	env[U"x"] = make_type<float>();
+	env[U"y"] = make_type<float>();
+
+	auto tr = expr::make<identifier_expr>( U"true" );
+	auto x = expr::make<identifier_expr>( U"x" );
+	auto y = expr::make<identifier_expr>( U"y" );
+	auto add = expr::make<infix_expr>( U"+", x, y );
+	auto end = expr::make<if_expr>( tr, add, y );
+
+	type t = infer( *end, env );
+
+	std::cout << end << ": " << t << std::endl;
+	*/
+
+	return 0;
+}
+
+}
+
+int main( int argc, char *argv[] )
+{
+	try
+	{
+		return safemain( argc, argv );
+	}
+	catch ( const std::exception &e )
+	{
+		base::print_exception( std::cerr, e );
+	}
+}
