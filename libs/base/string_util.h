d51b972ed38bf095ccc8b7ac75c7c5ded3e36587
Ian Godin <iangodin@gmail.com>
2015-03-24 21:37:44 -0700

    Added split function.

--- libs/base/string_util.h
+++ libs/base/string_util.h
@@ -11,12 +11,30 @@ namespace base
 ////////////////////////////////////////
 
 std::string trim( const std::string &str, const std::string &ws = " \t\n\r" );
+
+////////////////////////////////////////
+
+template<typename Inserter>
+inline void
+split( const std::string &str, char delim, Inserter insert )
+{
+	/// @todo Implement this in a more efficient way.
+	std::stringstream ss( str );
+	std::string item;
+	while ( std::getline( ss, item, delim ) )
+		insert = item;
+}
+
+////////////////////////////////////////
+
 std::string replace( std::string &&str, char c, const std::string &replacement );
 
 inline std::string replace( const std::string &str, char c, const std::string &replacement )
 {
 	return replace( std::string( str ), c, replacement );
 }
+
+////////////////////////////////////////
 
 template<typename T>
 std::string to_string( const T &t )
