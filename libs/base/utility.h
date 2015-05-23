96d68d13aed4d9b56b8e6854aa2812e74a0fcc2d
Kimball Thurston kdt3rd@gmail.com <kdt3rd@gmail.com>
2015-05-23 15:59:21 -0700

    start utility function wrapper

---
+++ libs/base/utility.h
@@ -0,0 +1,50 @@
+//
+// Copyright (c) 2015 Kimball Thurston
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the "Software"),
+// to deal in the Software without restriction, including without limitation
+// the rights to use, copy, modify, merge, publish, distribute, sublicense,
+// and/or sell copies of the Software, and to permit persons to whom the
+// Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included
+// in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+// CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+// OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+#pragma once
+
+#include <utility>
+
+
+////////////////////////////////////////
+
+
+namespace base
+{
+
+#if __cplusplus <= 201103L
+template <typename T, typename U = T>
+inline T
+exchange( T &obj, U &&nval )
+{
+	T ret = std::move( obj );
+	obj = std::forward<U>( nval );
+	return ret;
+}
+#else
+using std::exchange;
+#endif
+
+} // namespace base
+
+
+
