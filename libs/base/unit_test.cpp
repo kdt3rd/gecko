eb00ba396c2c52c9903a1c7056deffc24a118281
Ian Godin <iangodin@gmail.com>
2015-05-24 18:32:17 -0700

    Added unit_test class.

--- libs/base/unit_test.cpp
+++ libs/base/unit_test.cpp
@@ -2,56 +2,152 @@
 #include "unit_test.h"
 #include "ansi.h"
 #include "contract.h"
+#include "scope_guard.h"
+#include "reverse.h"
+#include <algorithm>
 
 namespace base
 {
 
 ////////////////////////////////////////
 
-unit_test::unit_test( const std::string &n, const std::function<void(void)> &t )
-	: _name( n ), _test( t )
+unit_test::unit_test( const std::string &n )
+	: _name( n )
 {
-	auto i = _tests.insert( std::make_pair( n, this ) );
-	if ( !i.second )
-		throw_runtime( "duplicate unit_test named '{0}'", n );
+	precondition( !_name.empty(), "unit test without name" );
 }
 
 ////////////////////////////////////////
 
 unit_test::~unit_test( void )
 {
-	_tests.erase( _name );
 }
 
 ////////////////////////////////////////
 
-void unit_test::test( void ) const
+void unit_test::setup( cmd_line &opts )
 {
-	try
-	{
-		_test();
-		std::clog << ansi::green << "SUCCESS " << ansi::reset << _name << std::endl;
-	}
-	catch ( std::exception &e )
-	{
-		std::clog << ansi::red << "FAILURE " << ansi::reset << _name << std::endl;
-		print_exception( std::clog, e, 1 );
-	}
+	opts.add( cmd_line::option( 't', "test_" + _name, "<test> ...", cmd_line::args, "List of " + _name + " tests to run" ));
+}
+
+////////////////////////////////////////
+
+void unit_test::success( const std::string &msg )
+{
+	std::clog << ansi::green << " SUCCESS " << ansi::reset << _running.back() << ": " << msg << std::endl;
+}
+
+////////////////////////////////////////
+
+void unit_test::failure( const std::string &msg )
+{
+	std::clog << ansi::red << " FAILURE " << ansi::reset << _running.back() << ": " << msg << std::endl;
+}
+
+////////////////////////////////////////
+
+void unit_test::warning( const std::string &msg )
+{
+	std::clog << ansi::yellow << " WARNING " << ansi::reset << _running.back() << ": " << msg << std::endl;
+}
+
+////////////////////////////////////////
+
+void unit_test::message( const std::string &msg )
+{
+	std::clog << ansi::blue << " MESSAGE " << ansi::reset << _running.back() << ": " << msg << std::endl;
 }
 
 ////////////////////////////////////////
 
 void unit_test::run( void )
 {
-	for ( auto &t: _tests )
-		t.second->test();
+	for ( auto i = _tests.begin(); i != _tests.end(); ++i )
+		run( i );
+}
+
+////////////////////////////////////////
+
+void unit_test::run( cmd_line &opts )
+{
+	std::string optname = "test_" + _name;
+	bool done = false;
+
+	if ( auto &opt = opts[optname.c_str()] )
+	{
+		if ( !opt.values().empty() )
+		{
+			for ( auto &n: opt.values() )
+				run( std::string( n ) );
+			done = true;
+		}
+	}
+
+	if ( !done )
+		run();
 }
 
 ////////////////////////////////////////
 
 void unit_test::run( const std::string &n )
 {
-	_tests.at( n )->test();
+	auto it = _tests.find( n );
+	if ( it == _tests.end() )
+		throw_runtime( "test '{0}' not found", n );
+	run( it );
+}
+
+////////////////////////////////////////
+
+void unit_test::run( std::map<std::string,std::function<void(void)>>::iterator &it )
+{
+	precondition( it != _tests.end(), "invalid test" );
+
+	// Make sure we are not already running...
+	const std::string &n = it->first;
+	if ( std::find( _running.begin(), _running.end(), n ) == _running.end() )
+		_running.emplace_back( n );
+	else
+		throw_runtime( "recursive tests not allowed: {0}", infix_separated( ", ", _running ) );
+	on_scope_exit { _running.erase( std::remove( _running.begin(), _running.end(), n ), _running.end() ); };
+
+	// Already completed
+	if ( _success.find( n ) != _success.end() )
+		return;
+	if ( _failure.find( n ) != _failure.end() )
+		throw_runtime( "test '{0}' has failed", n );
+
+	// Run the test
+	size_t failures = _failure.size();
+	try
+	{
+		it->second();
+	}
+	catch ( std::exception &e )
+	{
+		failure( std::string( e.what() ) );
+	}
+
+	if ( failures != _failure.size() )
+		_failure.insert( n );
+	else
+		_success.insert( n );
+}
+
+////////////////////////////////////////
+
+void unit_test::clean( void )
+{
+	for ( auto &c: base::reverse( _cleanup ) )
+	{
+		try
+		{
+			c();
+		}
+		catch ( ... )
+		{
+		}
+	}
 }
 
 ////////////////////////////////////////
--- libs/base/unit_test.h
+++ libs/base/unit_test.h
@@ -4,25 +4,109 @@
 #include <string>
 #include <functional>
 #include <map>
+#include <set>
+#include "format.h"
+#include "cmd_line.h"
 
 namespace base
 {
 
 ////////////////////////////////////////
 
+/// @brief Unit test
+///
+/// A Unit test with a name and a function.
+/// If this function throws an exception, the test will have failed.
+/// The function should use the test_false() and test_true() (below) to show results.
+/// It can also call success() and failure() explicitly.
+/// Extra messages can also be displayed with warnin() and message().
 class unit_test
 {
 public:
-	unit_test( const std::string &n, const std::function<void(void)> &t );
+	unit_test( const std::string &n );
 	~unit_test( void );
 
-	void test( void ) const;
+	void setup( cmd_line &opts );
 
-	static void run( void );
-	static void run( const std::string &n );
+	std::function<void(void)> &operator[]( const std::string &n )
+	{
+		return _tests[n];
+	}
+
+	template<typename ...Args>
+	void test( bool v, Args ...args )
+	{
+		if ( v )
+			success( args... );
+		else
+			failure( args... );
+	}
+
+	void success( const std::string &msg );
+	void failure( const std::string &msg );
+	void warning( const std::string &msg );
+	void message( const std::string &msg );
+
+	template<typename ...Args>
+	void success( Args ...args )
+	{
+		success( std::string( format( std::forward<Args>( args )... ) ) );
+	}
+
+	template<typename ...Args>
+	void failure( Args ...args )
+	{
+		failure( std::string( format( std::forward<Args>( args )... ) ) );
+	}
+
+	template<typename ...Args>
+	void warning( Args ...args )
+	{
+		warning( std::string( format( std::forward<Args>( args )... ) ) );
+	}
+
+	template<typename ...Args>
+	void message( Args ...args )
+	{
+		message( std::string( format( std::forward<Args>( args )... ) ) );
+	}
+
+	void run( void );
+	void run( const std::string &n );
+	void run( cmd_line &opts );
+
+	void clean( void );
+
+	size_t success_count( void ) const
+	{
+		return _success.size();
+	}
+
+	size_t failure_count( void ) const
+	{
+		return _failure.size();
+	}
+
+	size_t run_count( void ) const
+	{
+		return _success.size() + _failure.size();
+	}
+
+	std::function<void(void)> &cleanup( void )
+	{
+		_cleanup.emplace_back();
+		return _cleanup.back();
+	}
 
 private:
-	static std::map<std::string,unit_test*> _tests;
+	void run( std::map<std::string,std::function<void(void)>>::iterator &it );
+
+	std::map<std::string,std::function<void(void)>> _tests;
+	std::set<std::string> _success;
+	std::set<std::string> _failure;
+	std::vector<std::string> _running;
+
+	std::vector<std::function<void(void)>> _cleanup;
 
 	std::string _name;
 	std::function<void(void)> _test;
