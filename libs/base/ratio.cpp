32c371bcd66060b99bdf4f46cbb72f0f69a449b5
Kimball Thurston <kdt3rd@gmail.com>
2016-01-31 14:41:12 -0800

    fill out implementation of ratio class

--- libs/base/ratio.cpp
+++ libs/base/ratio.cpp
@@ -1,9 +1,147 @@
 
 #include "ratio.h"
 #include "math_functions.h"
+#include "contract.h"
 
 namespace base
 {
+
+////////////////////////////////////////
+
+ratio::ratio( int64_t n, int64_t d )
+		: _num( n ), _den( d )
+{
+	simplify();
+}
+
+////////////////////////////////////////
+
+void ratio::set( int64_t n, int64_t d )
+{
+	_num = n;
+	_den = d;
+	simplify();
+}
+
+////////////////////////////////////////
+
+std::pair<ratio,ratio> ratio::common( const ratio &r ) const
+{
+	precondition( valid() && r.valid(), "unable to find common denominator for invalid ratios" );
+
+	int64_t newnumA = math::gcd( numerator() * r.denominator(), r.numerator() * denominator() );
+	int64_t newnumB = math::gcd( r.numerator() * denominator(), numerator() * r.denominator() );
+	int64_t newden = denominator() * r.denominator();
+
+	// constructor will simplify to prevent
+	// numbers growing
+	return std::make_pair( ratio( newnumA, newden ),
+						   ratio( newnumB, newden ) );
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator +=( const ratio &r )
+{
+	if ( denominator() == r.denominator() )
+	{
+		_num += r.numerator();
+	}
+	else
+	{
+		std::pair<ratio,ratio> c = common( r );
+
+		_num = c.first.numerator() + c.second.numerator();
+		_den = c.first.denominator();
+	}
+	simplify();
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator -=( const ratio &r )
+{
+	if ( denominator() == r.denominator() )
+	{
+		_num -= r.numerator();
+	}
+	else
+	{
+		std::pair<ratio,ratio> c = common( r );
+
+		_num = c.first.numerator() - c.second.numerator();
+		_den = c.first.denominator();
+	}
+	simplify();
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator *=( const ratio &r )
+{
+	_num *= r.numerator();
+	_den *= r.denominator();
+	simplify();
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator /=( const ratio &r )
+{
+	_num *= r.denominator();
+	_den *= r.numerator();
+	simplify();
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator +=( int64_t n )
+{
+	if ( denominator() == 1 )
+		_num += n;
+	else
+	{
+		_num += n * denominator();
+		simplify();
+	}
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator -=( int64_t n )
+{
+	if ( denominator() == 1 )
+		_num -= n;
+	else
+	{
+		_num -= n * denominator();
+		simplify();
+	}
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator *=( int64_t n )
+{
+	_num *= n;
+	simplify();
+	return *this;
+}
+
+////////////////////////////////////////
+
+ratio &ratio::operator /=( int64_t n )
+{
+	_den *= n;
+	simplify();
+	return *this;
+}
 
 ////////////////////////////////////////
 
@@ -15,6 +153,92 @@ void ratio::simplify( void )
 		_num /= d;
 		_den /= d;
 	}
+}
+
+////////////////////////////////////////
+
+ratio operator+( const ratio &a, const ratio &b )
+{
+	ratio t( a );
+	t += b;
+	return t;
+}
+
+ratio operator+( const ratio &a, int64_t b )
+{
+	ratio t( a );
+	t += b;
+	return t;
+}
+
+ratio operator+( int64_t a, const ratio &b )
+{
+	ratio t( b );
+	t += a;
+	return t;
+}
+
+ratio operator-( const ratio &a, const ratio &b )
+{
+	ratio t( a );
+	t -= b;
+	return t;
+}
+
+ratio operator-( const ratio &a, int64_t b )
+{
+	ratio t( a );
+	t -= b;
+	return t;
+}
+
+ratio operator-( int64_t a, const ratio &b )
+{
+	ratio t( a, int64_t(1) );
+	t -= b;
+	return t;
+}
+
+ratio operator*( const ratio &a, const ratio &b )
+{
+	ratio t( a );
+	t *= b;
+	return t;
+}
+
+ratio operator*( const ratio &a, int64_t b )
+{
+	ratio t( a );
+	t *= b;
+	return t;
+}
+
+ratio operator*( int64_t a, const ratio &b )
+{
+	ratio t( b );
+	t *= a;
+	return t;
+}
+
+ratio operator/( const ratio &a, const ratio &b )
+{
+	ratio t( a );
+	t /= b;
+	return t;
+}
+
+ratio operator/( const ratio &a, int64_t b )
+{
+	ratio t( a );
+	t /= b;
+	return t;
+}
+
+ratio operator/( int64_t a, const ratio &b )
+{
+	ratio t( a, int64_t(1) );
+	t /= b;
+	return t;
 }
 
 ////////////////////////////////////////
--- libs/base/ratio.h
+++ libs/base/ratio.h
@@ -3,6 +3,7 @@
 
 #include <cstdint>
 #include <iostream>
+#include <utility>
 
 namespace base
 {
@@ -12,36 +13,148 @@ namespace base
 class ratio
 {
 public:
-	ratio( int64_t n, int64_t d )
-		: _num( n ), _den( d )
-	{
-		simplify();
-	}
+	ratio( void ) = default;
+	ratio( int64_t n, int64_t d );
+	ratio( const ratio & ) = default;
+	ratio( ratio && ) = default;
+	~ratio( void ) = default;
+	ratio &operator=( const ratio & ) = default;
+	ratio &operator=( ratio && ) = default;
 
-	int64_t numerator( void ) const
-	{
-		return _num;
-	}
+	void set( int64_t n, int64_t d );
 
-	int64_t denominator( void ) const
-	{
-		return _den;
-	}
+	inline bool valid( void ) const;
 
-	double value( void ) const
-	{
-		return double(_num) / double(_den);
-	}
+	inline int64_t numerator( void ) const;
+
+	inline int64_t denominator( void ) const;
+
+	inline double value( void ) const;
+
+	inline ratio invert( void ) const;
+	inline int64_t trunc( void ) const;
+	inline int64_t mod( void ) const;
+	inline int64_t round( void ) const;
+
+	ratio &operator +=( const ratio &r );
+	ratio &operator -=( const ratio &r );
+	ratio &operator *=( const ratio &r );
+	ratio &operator /=( const ratio &r );
+	ratio &operator +=( int64_t n );
+	ratio &operator -=( int64_t n );
+	ratio &operator *=( int64_t n );
+	ratio &operator /=( int64_t n );
+
+	/// @brief Return a new pair of ratios with a common denominator
+	std::pair<ratio,ratio> common( const ratio &r ) const;
 
 private:
 	void simplify( void );
 
-	int64_t _num;
-	int64_t _den;
+	int64_t _num = 0;
+	int64_t _den = 0;
 };
 
 ////////////////////////////////////////
 
+inline bool ratio::valid( void ) const
+{
+	return _den != 0;
+}
+
+inline int64_t ratio::numerator( void ) const
+{
+	return _num;
+}
+
+inline int64_t ratio::denominator( void ) const
+{
+	return _num;
+}
+
+inline double ratio::value( void ) const
+{
+	return static_cast<double>(_num) / static_cast<double>(_den);
+}
+
+inline ratio ratio::invert( void ) const
+{
+	return ratio( _den, _num );
+}
+
+inline int64_t ratio::trunc( void ) const
+{
+	return _num / _den;
+}
+
+inline int64_t ratio::mod( void ) const
+{
+	return _num % _den;
+}
+
+inline int64_t ratio::round( void ) const
+{
+	return ( _num + ( _num < 0 ? -_den : _den ) / 2 ) / _den;
+}
+
+////////////////////////////////////////
+
+/// @defgroup RatioMath ratio related simple math operators
+///
+/// @{
+ratio operator+( const ratio &a, const ratio &b );
+ratio operator+( const ratio &a, int64_t b );
+ratio operator+( int64_t a, const ratio &b );
+ratio operator-( const ratio &a, const ratio &b );
+ratio operator-( const ratio &a, int64_t b );
+ratio operator-( int64_t a, const ratio &b );
+ratio operator*( const ratio &a, const ratio &b );
+ratio operator*( const ratio &a, int64_t b );
+ratio operator*( int64_t a, const ratio &b );
+ratio operator/( const ratio &a, const ratio &b );
+ratio operator/( const ratio &a, int64_t b );
+ratio operator/( int64_t a, const ratio &b );
+/// @}
+
+////////////////////////////////////////
+
+/// @defgroup RatioComparison ratio related comparison routines
+///
+/// @{
+inline bool operator==( const ratio &a, const ratio &b )
+{
+	return a.numerator() * b.denominator() == b.numerator() * a.denominator();
+}
+
+inline bool operator!=( const ratio &a, const ratio &b )
+{
+	return !( a == b );
+}
+
+inline bool operator<( const ratio &a, const ratio &b )
+{
+	return a.numerator() * b.denominator() < b.numerator() * a.denominator();
+}
+
+inline bool operator<=( const ratio &a, const ratio &b )
+{
+	return a.numerator() * b.denominator() <= b.numerator() * a.denominator();
+}
+
+inline bool operator>( const ratio &a, const ratio &b )
+{
+	return a.numerator() * b.denominator() > b.numerator() * a.denominator();
+}
+
+inline bool operator>=( const ratio &a, const ratio &b )
+{
+	return a.numerator() * b.denominator() >= b.numerator() * a.denominator();
+}
+/// @}
+
+////////////////////////////////////////
+
+/// @brief stream operator
 inline std::ostream &operator<<( std::ostream &out, const ratio &r )
 {
 	out << r.numerator() << '/' << r.denominator();
