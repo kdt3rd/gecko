993be18f35c90bb9df2eecd73641c373b6cfa72b
Kimball Thurston <kdt3rd@gmail.com>
2016-03-26 18:49:49 -0700

    add extension retrieval utility

--- libs/base/file_path.cpp
+++ libs/base/file_path.cpp
@@ -6,10 +6,20 @@ namespace base
 
 ////////////////////////////////////////
 
+std::string get_extension( const std::string &file )
+{
+	size_t off = file.find_last_of( '.' );
+	if ( off != std::string::npos )
+		return file.substr( off + 1 );
+	return std::string();
+}
+
+////////////////////////////////////////
+
 std::string remove_extension( const std::string &file )
 {
 	size_t off = file.find_last_of( '.' );
-	if ( off < file.size() )
+	if ( off != std::string::npos )
 		return file.substr( 0, off );
 	return file;
 }
@@ -19,7 +29,7 @@ std::string remove_extension( const std::string &file )
 std::string remove_extension( std::string &&file )
 {
 	size_t off = file.find_last_of( '.' );
-	if ( off < file.size() )
+	if ( off != std::string::npos )
 		file.erase( off );
 	return std::move( file );
 }
@@ -29,13 +39,13 @@ std::string remove_extension( std::string &&file )
 std::string basename( const std::string &file )
 {
 	size_t start = file.find_last_of( '/' );
-	if ( start > file.size() )
+	if ( start == std::string::npos )
 		start = 0;
 	else
 		start = start + 1;
 
 	size_t end = file.find_last_of( '.' );
-	if ( end > file.size() )
+	if ( end == std::string::npos )
 		end = file.size();
 
 	return file.substr( start, end - start );
--- libs/base/file_path.h
+++ libs/base/file_path.h
@@ -8,6 +8,10 @@ namespace base
 
 ////////////////////////////////////////
 
+/// if the file name is blah.exr, the extension
+/// returned will be 'exr'
+std::string get_extension( const std::string &file );
+
 std::string remove_extension( const std::string &file );
 std::string remove_extension( std::string &&file );
 
