7d1631438f87a5ff7f4f4c9b81e869e42353c754
Kimball Thurston <kdt3rd@gmail.com>
2017-10-01 23:40:57 +1300

    add dso loadable abstraction

---
+++ libs/base/loadable.cpp
@@ -0,0 +1,119 @@
+//
+// Copyright (c) 2017 Kimball Thurston
+// All rights reserved.
+// Copyrights licenced under the MIT License.
+// See the accompanying LICENSE.txt file for terms
+//
+
+#ifdef _WIN32
+# include <windows.h>
+#else
+# ifndef _GNU_SOURCE
+#  define _GNU_SOURCE
+# endif
+# include <dlfcn.h>
+# include <atomic>
+#endif
+#include "loadable.h"
+#include "contract.h"
+
+////////////////////////////////////////
+
+namespace base
+{
+
+////////////////////////////////////////
+
+loadable::loadable( std::string lpath, isolation iso )
+	: _loadpath( std::move( lpath ) )
+{
+#ifdef _WIN32
+	HMODULE x = LoadLibrary( static_cast<LPCTSTR>( _loadpath.c_str() ) );
+	if ( x == NULL )
+		throw_lasterror( "Unable to load '{0}'", lpath );
+	_dso.reset( static_cast<void *>( x ), []( void *p ) { FreeLibrary( reinterpret_cast<HMODULE>(p) ); } );
+#else
+	// TODO: Add a flag for RTLD_NOW vs RTLD_LAZY?
+	void *d = nullptr;
+	if ( iso == isolation::ISOLATE )
+	{
+		static std::atomic<Lmid_t> h_namespace( LM_ID_NEWLM );
+		Lmid_t ns = h_namespace.load();
+
+		d = dlmopen( ns, _loadpath.c_str(), RTLD_LAZY | RTLD_LOCAL );
+		if ( d )
+		{
+			if ( ns == LM_ID_NEWLM )
+			{
+				if ( dlinfo( d, RTLD_DI_LMID, &ns ) == -1 )
+				{
+					std::string err = dlerror();
+					dlclose( d );
+					throw_runtime( "Unable to query namespace id: {0}", err );
+				}
+				
+				h_namespace.store( ns );
+			}
+		}
+	}
+	else if ( iso == isolation::GLOBALIZE )
+		d = dlopen( _loadpath.c_str(), RTLD_LAZY | RTLD_GLOBAL );
+	else if ( iso == isolation::NORMAL )
+		d = dlopen( _loadpath.c_str(), RTLD_LAZY | RTLD_LOCAL );
+
+	if ( ! d )
+	{
+		std::string err = dlerror();
+		throw_runtime( "Unable to open DSO: {0}", err );
+	}
+
+	_dso.reset( d, dlclose );
+#endif
+}
+
+////////////////////////////////////////
+
+loadable::~loadable( void )
+{
+}
+
+////////////////////////////////////////
+
+void *
+loadable::symbol( const const_string<char> &symName, const cstring &symVersion )
+{
+#ifdef _WIN32
+	return GetProcAddress( reinterpret_cast<HMODULE>( _dso.get() ), symName.c_str() );
+#else
+	if ( symVersion.empty() )
+		return dlsym( _dso.get(), symName.c_str() );
+	return dlvsym( _dso.get(), symName.c_str(), symVersion.c_str() );
+#endif
+}
+
+////////////////////////////////////////
+
+void *
+loadable::find_app_symbol( const cstring &symName, const cstring &symVersion )
+{
+	if ( symVersion.empty() )
+		return dlsym( RTLD_DEFAULT, symName.c_str() );
+	return dlvsym( RTLD_DEFAULT, symName.c_str(), symVersion.c_str() );
+}
+
+////////////////////////////////////////
+
+void *
+loadable::find_next_symbol( const cstring &symName, const cstring &symVersion )
+{
+	if ( symVersion.empty() )
+		return dlsym( RTLD_NEXT, symName.c_str() );
+	return dlvsym( RTLD_NEXT, symName.c_str(), symVersion.c_str() );
+}
+
+////////////////////////////////////////
+
+} // base
+
+
+
---
+++ libs/base/loadable.h
@@ -0,0 +1,60 @@
+//
+// Copyright (c) 2017 Kimball Thurston
+// All rights reserved.
+// Copyrights licenced under the MIT License.
+// See the accompanying LICENSE.txt file for terms
+//
+
+#pragma once
+
+#include "const_string.h"
+#include <memory>
+
+////////////////////////////////////////
+
+namespace base
+{
+
+///
+/// @brief Class loadable provides a wrapper around a dynamically
+/// loaded executable file (i.e. plugin or other dso).
+///
+/// Linux, Solaris, et al. provide an isolation mechanism
+/// (dlmopen). However, windows does not seem to provide such a
+/// mechanism via the generic API. However, linking has to be
+/// hard-bound under windows, so in practice this is not the same
+/// issue.
+///
+/// There is a limited amount of isolation slots available to dlmopen (16 under glibc), so rather than a generic flag
+///
+class loadable
+{
+public:
+	enum class isolation
+	{
+		NORMAL,
+		GLOBALIZE,
+		ISOLATE
+	};
+
+	loadable( std::string lpath, isolation iso = isolation::NORMAL );
+	loadable( void ) = default;
+	loadable( const loadable & ) = default;
+	loadable( loadable && ) = default;
+	loadable &operator=( const loadable & ) = default;
+	loadable &operator=( loadable && ) = default;
+	~loadable( void );
+
+	void *symbol( const cstring &symName, const cstring &symVersion = cstring() );
+
+	static void *find_app_symbol( const cstring &symName, const cstring &symVersion = cstring() );
+	static void *find_next_symbol( const cstring &symName, const cstring &symVersion = cstring() );
+private:
+	std::string _loadpath;
+	std::shared_ptr<void> _dso;
+};
+
+} // namespace base
+
+
+
