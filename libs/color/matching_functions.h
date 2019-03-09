c1c0dcfa3783e3721e2ec6b120d87fae9ed7b8cb
Kimball Thurston <kdt3rd@gmail.com>
2019-03-09 18:39:17 +1300

    Add initial 1931 2 degree color matching function table
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- libs/color/matching_functions.h
+++ libs/color/matching_functions.h
@@ -17,32 +17,33 @@ namespace two_deg
 {
 
 const matching_function &cie_1931_cmf( void );
-const matching_function &cie_1931_judd_1951( void );
-const matching_function &cie_1931_judd_vos( void );
+//const matching_function &cie_1931_judd_1951( void );
+//const matching_function &cie_1931_judd_vos( void );
 
 /// LMS cone fundamental intensity functions (not yet ratified?)
 /// x == l, y == m, z == s
-const matching_function &cie_2012_xyz_cmf( void );
-const luminous_efficiency_function &stockman_2005_Vlambda( void );
-const luminous_efficiency_function &stockman_2011_Vlambda( void );
+
+//const matching_function &cie_2012_xyz_cmf( void );
+//const luminous_efficiency_function &stockman_2005_Vlambda( void );
+//const luminous_efficiency_function &stockman_2011_Vlambda( void );
 
 }
 
-const luminous_efficiency_function &cie_1924_photopic_Vlambda( void );
-const luminous_efficiency_function &cie_1924_photopic_Vlambda_judd_1951( void );
-const luminous_efficiency_function &cie_1924_photopic_Vlambda_judd_vos( void );
+//const luminous_efficiency_function &cie_1924_photopic_Vlambda( void );
+//const luminous_efficiency_function &cie_1924_photopic_Vlambda_judd_1951( void );
+//const luminous_efficiency_function &cie_1924_photopic_Vlambda_judd_vos( void );
 
 namespace ten_deg
 {
 
-const matching_function &cie_1964_cmf( void );
+//const matching_function &cie_1964_cmf( void );
 
 /// LMS cone fundamental intensity function (not yet ratified)
 /// as for two deg
-const matching_function &cie_2012_xyz_cmf( void );
+//const matching_function &cie_2012_xyz_cmf( void );
 
-const luminous_efficiency_function &stockman_2005_Vlambda( void );
-const luminous_efficiency_function &stockman_2011_Vlambda( void );
+//const luminous_efficiency_function &stockman_2005_Vlambda( void );
+//const luminous_efficiency_function &stockman_2011_Vlambda( void );
 }
 
 } // namespace color
