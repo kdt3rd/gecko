cdb6097341e040972a6b584b300f34a7b0a56990
Kimball Thurston <kdt3rd@gmail.com>
2019-09-15 19:33:35 +1200

    Finish initial implementation of string pool object
    Signed-off-by: Kimball Thurston <kdt3rd@gmail.com>

--- libs/base/string_pool.cpp
+++ libs/base/string_pool.cpp
@@ -9,12 +9,44 @@
 
 namespace base
 {
+
+////////////////////////////////////////
+
+size_tagged_string string_pool::create( const cstring &s )
+{
+    {
+        std::shared_lock<std::shared_timed_mutex> lk( _mutex );
+        auto i = _pre_created.find( s );
+        if ( i != _pre_created.end() )
+            return i->second;
+    }
+    return add( s );
+}
+
+size_tagged_string string_pool::create( const char *s, const char *end )
+{
+    return create( cstring( s, end ) );
+}
+
+std::pair<size_t, size_t> string_pool::clear( void )
+{
+    std::lock_guard<std::shared_timed_mutex> lk( _mutex );
+    _pre_created.clear();
+    return _strings.clear();
+}
+
+////////////////////////////////////////
+
+char *string_pool::alloc( size_t len ) { return _strings.get<char>( len ); }
+
+////////////////////////////////////////
+
 size_tagged_string
-size_tagged_string::create( string_pool &pool, const char *src, size_t len )
+string_pool::make( const char *src, size_t len )
 {
     if ( len < 128 )
     {
-        char *ptr = pool.alloc( len + 2 );
+        char *ptr = alloc( len + 2 );
         ptr[0]    = static_cast<char>( len );
         std::copy( src, src + len, ptr + 1 );
         ptr[len + 1] = '\0';
@@ -23,7 +55,7 @@ size_tagged_string::create( string_pool &pool, const char *src, size_t len )
 
     if ( len < 256 )
     {
-        char *ptr                               = pool.alloc( len + 3 );
+        char *ptr                               = alloc( len + 3 );
         *( reinterpret_cast<uint8_t *>( ptr ) ) = static_cast<uint8_t>( len );
         ++ptr;
         *ptr++ = static_cast<char>( -1 );
@@ -34,7 +66,7 @@ size_tagged_string::create( string_pool &pool, const char *src, size_t len )
 
     if ( len < 65536 )
     {
-        char *ptr                                = pool.alloc( len + 4 );
+        char *ptr                                = alloc( len + 4 );
         *( reinterpret_cast<uint16_t *>( ptr ) ) = static_cast<uint16_t>( len );
         ptr += 2;
         *ptr++ = static_cast<char>( -2 );
@@ -45,7 +77,7 @@ size_tagged_string::create( string_pool &pool, const char *src, size_t len )
 
     if ( len > std::numeric_limits<uint32_t>::max() )
     {
-        char *ptr                                = pool.alloc( len + 10 );
+        char *ptr                                = alloc( len + 10 );
         *( reinterpret_cast<uint64_t *>( ptr ) ) = static_cast<uint64_t>( len );
         ptr += 8;
         *ptr++ = static_cast<char>( -8 );
@@ -54,7 +86,7 @@ size_tagged_string::create( string_pool &pool, const char *src, size_t len )
         return size_tagged_string( ptr );
     }
 
-    char *ptr                                = pool.alloc( len + 6 );
+    char *ptr                                = alloc( len + 6 );
     *( reinterpret_cast<uint32_t *>( ptr ) ) = static_cast<uint32_t>( len );
     ptr += 4;
     *ptr++ = static_cast<char>( -4 );
@@ -65,8 +97,16 @@ size_tagged_string::create( string_pool &pool, const char *src, size_t len )
 
 ////////////////////////////////////////
 
-char *string_pool::alloc( size_t len ) { return _strings.get<char>( len ); }
+size_tagged_string string_pool::add( const cstring &s )
+{
+    std::lock_guard<std::shared_timed_mutex> lk( _mutex );
+    // someone might have gotten here before us, avoid double creation
+    auto i = _pre_created.find( s );
+    if ( i != _pre_created.end() )
+        return i->second;
 
-////////////////////////////////////////
-
+    size_tagged_string n = make( s.c_str(), s.size() );
+    _pre_created[cstring(n.c_str(), n.size())] = n;
+    return n;
+}
 } // namespace base
--- libs/base/string_pool.h
+++ libs/base/string_pool.h
@@ -5,9 +5,15 @@
 
 #include "concurrent_mem_pool.h"
 
+#include "const_string.h"
+
+#include "fnv_hash.h"
+
 #include <cstring>
 #include <ostream>
 #include <string>
+#include <shared_mutex>
+#include <unordered_map>
 
 ////////////////////////////////////////
 
@@ -23,35 +29,37 @@ public:
 
     size_t size( void ) const
     {
+        if ( empty() )
+            return 0;
+
         signed char sz = reinterpret_cast<const signed char *>( _str )[-1];
         switch ( sz )
         {
-            case -8:
+            case -8: // errr, yeah, big
                 return static_cast<size_t>(
                     *( reinterpret_cast<const uint64_t *>( _str - 9 ) ) );
-            case -4:
+            case -4: // 65536-(2^32 - 1)
                 return static_cast<size_t>(
                     *( reinterpret_cast<const uint32_t *>( _str - 5 ) ) );
-            case -2:
+            case -2: // 256-65535
                 return static_cast<size_t>(
                     *( reinterpret_cast<const uint16_t *>( _str - 3 ) ) );
-            case -1:
+            case -1: // 128-255
                 return static_cast<size_t>(
                     *( reinterpret_cast<const uint8_t *>( _str - 2 ) ) );
-                break;
+            // 0-127
             default: return static_cast<size_t>( sz );
         }
     }
+
     bool        empty( void ) const { return _str == nullptr; }
     const char *c_str( void ) const { return _str; }
+    char        operator[]( size_t i ) const { return _str[i]; }
 
     explicit operator std::string( void ) const
     {
         return std::string( c_str(), size() );
     }
-
-    static size_tagged_string
-    create( string_pool &p, const char *src, size_t len );
 
 private:
     const char *_str = nullptr;
@@ -62,6 +70,9 @@ private:
 inline bool
 operator==( const size_tagged_string &a, const size_tagged_string &b )
 {
+    if ( a.c_str() == b.c_str() )
+        return true;
+
     return a.size() == b.size() &&
            std::memcmp( a.c_str(), b.c_str(), a.size() * sizeof( char ) ) == 0;
 }
@@ -112,6 +123,8 @@ inline bool operator!=( const size_tagged_string &sts, const std::string &s )
 inline bool
 operator<( const size_tagged_string &a, const size_tagged_string &b )
 {
+    if ( a.c_str() == b.c_str() )
+        return false;
     return a.size() < b.size() ||
            ( a.size() == b.size() &&
              std::memcmp( a.c_str(), b.c_str(), a.size() * sizeof( char ) ) <
@@ -154,6 +167,33 @@ inline std::ostream &operator<<( std::ostream &os, const size_tagged_string &s )
     return os;
 }
 
+template <typename T>
+inline fnv1a<T> &operator<<( fnv1a<T> &h, const size_tagged_string &v )
+{
+    h.add( v.c_str(), v.size() * sizeof( char ) );
+    return h;
+}
+
+} // namespace base
+
+namespace std
+{
+// overload std::hash
+template <> struct hash<base::size_tagged_string>
+{
+    typedef base::size_tagged_string argument_type;
+    typedef std::size_t              result_type;
+    inline result_type operator()( argument_type const &s ) const noexcept
+    {
+        base::fnv1a<result_type> hfunc;
+        hfunc.add( s.c_str(), s.size() * sizeof( char ) );
+        return static_cast<result_type>( hfunc.hash() );
+    }
+};
+} // namespace std
+
+namespace base
+{
 ////////////////////////////////////////////////////////////////////////////////
 
 class string_pool
@@ -165,30 +205,21 @@ public:
     string_pool( string_pool && )                 = delete;
     string_pool &operator=( string_pool && ) = delete;
 
-    size_tagged_string create( const char *s )
-    {
-        return size_tagged_string::create( *this, s, std::strlen( s ) );
-    }
-
-    size_tagged_string create( const std::string &s )
-    {
-        return size_tagged_string::create( *this, s.c_str(), s.size() );
-    }
-
-    size_tagged_string create( const char *s, const char *end )
-    {
-        return size_tagged_string::create(
-            *this, s, static_cast<size_t>( end - s ) );
-    }
-
-    char *alloc( size_t len );
+    size_tagged_string create( const cstring &s );
+    size_tagged_string create( const char *s, const char *end );
 
     /// NB: assumes all size_tagged_string objects are gone
     /// that refer to this string pool
-    std::pair<size_t, size_t> clear( void ) { return _strings.clear(); }
+    std::pair<size_t, size_t> clear( void );
 
 private:
-    concurrent_mem_pool<1> _strings;
+    char *             alloc( size_t len );
+    size_tagged_string make( const char *src, size_t len );
+    size_tagged_string add( const cstring &s );
+
+    concurrent_mem_pool<1>                          _strings;
+    std::shared_timed_mutex                         _mutex;
+    std::unordered_map<cstring, size_tagged_string> _pre_created;
 };
 
 } // namespace base
